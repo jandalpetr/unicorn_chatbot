2d463ce10f93ccddb64eb7a3eb9a603d0ffd26d7
jandalpetr <63299452+jandalpetr@users.noreply.github.com>
2020-12-16 11:36:04 +0100

    initial commit

---
+++ messages.js
@@ -0,0 +1,32 @@
+const messageEntry =[
+  { language: "en", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "uk", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "us", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "ie", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "ca", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "ca", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "au", text: "Hello, I think, you wrote to me in English. Or I could not find language you speak. I can talk only in English. Please write Hi or Hello to continue in English"},
+  { language: "cs", text: "Vítám Vás. Myslím, že píšete česky. Já umím pouze Anglicky. Napište Hi nebo Hello at můžeme pokračovat Anglicky ."},
+  { language: "de", text: "Hallo, ich glaube Sie schreiben mir Deutsch. I kann nur English. Bitte, schreibt Ihr mir Hi oder Hello so wir konnen English unterhalten."},
+  { language: "at", text: "Hallo, ich glaube Sie schreiben mir Deutsch. I kann nur English. Bitte, schreibt Ihr mir Hi oder Hello so wir konnen English unterhalten."},
+  { language: "da", text: "Hej, jeg tror, du skrev til mig på engelsk. Jeg kan kun tale på engelsk. Skriv Hej eller Hej for at fortsætte på engelsk."},
+  { language: "hu", text: "Helló, azt hiszem, magyarul írtál nekem. Csak angolul tudok beszélni. Írja be a Hi vagy a Hello szót, hogy folytassa angolul."},
+  { language: "es", text: "¡Hola!, creo que me escribiste en Español. Solo hablo Inglés. Por favor,  escriba Hi or Hello para continuar en Inglés."},
+  { language: "it", text: "Ciao, credo, mi hai scritto in italiano. Posso parlare solo in inglese. Per favore scrivi Hi o Hello per continuare l'inglese. it"},
+  { language: "pt", text: "Olá, eu acho, você escreveu para mim em português. Eu posso falar apenas em inglês. Por favor, escreva Hi ou Hello para continuar em Inglês"},
+  { language: "ro", text: "Salut, cred, mi-ai scris în română. Pot vorbi numai în engleză. Vă rugăm să scrieți Hi sau Hello ziua pentru a continua limba engleză it"},
+  { language: "sk", text: "Vítám Vás. Myslím, že píšete slovensky. Já umím pouze Anglicky. Napíšte Hi nebo Hello a môžeme pokračovať Anglicky"},
+  { language: "pl", text: "Witaj. Myślę, że piszesz po polsku. Mogę mówić tylko po angielsku. Napisz Hi lub Hello i możemy kontynuować w języku angielskim"},
+  { language: "bg", text: "Здравейте, за съжаление може да кумуникираме само на енглийски език. Моля, напишете Hi или Hello и след това може да продължим на английски."},
+  { language: "cr", text: "Dobrodošli. Mislim da pišete hrvatski. Govorim samo engleski. Napišite Hi ili Hello i možemo nastaviti na engleskom"},
+  { language: "fr", text: "Bonjour, je pense, tu m'as écrit en français. Je ne peux parler qu'en anglais. Veuillez écrire Hi ou Hello pour continuer l'anglais it"},
+  { language: "lv", text: "Sveiki, es domāju, ka jūs man rakstījāt latviski. Es varu runāt tikai angļu valodā. Lūdzu, rakstiet Hi vai Hello, lai turpinātu angļu valodu"},
+  { language: "lt", text: "Sveiki, manau, tu man parašei lietuviškai. Aš moku kalbėti tik angliškai. Parašykite Hi arba Hello, jei norite tęsti anglų kalbą."},
+  { language: "ee", text: "Tere, ma arvan, et sa kirjutasid mulle eesti keeles. Saan rääkida ainult inglise keeles. Palun kirjutage Hi või Hello, et jätkata inglise keelt"},
+  { language: "ua", text: "Ласкаво просимо. Думаю, ви пишете Український. Я можу говорити лише англійською. Напишіть Hi чи Hello, і ми можемо продовжувати англійською"},
+  { language: "fi", text: "Hei, luulen, kirjoitit minulle suomeksi. Voin puhua vain englanniksi. Jatka englannin kirjoittamista kirjoittamalla Hi tai Hello"},
+  { language: "sl", text: "Dobrodošli. Mislim, da pišete slovensko. Govorim lahko samo angleško. Napišite Hi ali Hello in lahko nadaljujemo v angleščini"}
+  ];
+
+  // Exporting variables and functions
+export { messageEntry };
